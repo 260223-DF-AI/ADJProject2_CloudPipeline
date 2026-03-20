3b5f68d866277f5853852b68ccc8daa3cb01e5bc
Jaisal <jkmehta@iu.edu>
2026-03-20 15:16:16 -0500

    Enhanced CSV to Parquet conversion with logging/error handling and added google.cloud imports

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from .routers import apiroutes
+from upload import csv_to_parquet, FILE_PATHS, OUTPUT_FILE
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI(
     title = "Placeholder API",
@@ -14,8 +19,9 @@ def get_root():
     return {"message": "Hello from main"}
 
 
-@app.post("/")
+@app.post("/convert")
 def post_root():
     pass
+    # csv_to_parquet(FILE_PATHS, OUTPUT_FILE)
 
 # run: uvicorn app.main:app --reload
