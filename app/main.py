78115d1d7bc54f598c0aba6eb073d6ee539a5cfe
Jaisal <jkmehta@iu.edu>
2026-03-23 19:42:38 -0500

    Added few analysis functions in main.py, fixed post_root function to include 5th csv source file

--- app/main.py
+++ app/main.py
@@ -48,10 +48,45 @@ async def get_item_test(item_id: int, bq_client: bigquery.client.Client = Depend
         raise HTTPException(status_code=500, detail=f"BigQuery query failed: {e}")
     return result.to_dict(orient="records")
 
+@app.get("/top-n-products")
+def get_top_N_products(n: int, bq_client: bigquery.client.Client = Depends(get_bq_client)):
+    """
+    Handles GET requests to the gcs-query URL and queries BigQuery.
+    """
+    query = f"""
+            select ProductName, TotalAmount
+            from `project2-cloudpipeline.sales_dataset.sales-data`
+            order by TotalAmount desc
+            limit {n}
+            """
+    try:
+        bq_client = bigquery.Client()
+        query_job = bq_client.query(query)
+        result = query_job.result().to_dataframe()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"BigQuery query failed: {e}")
+    return result.to_dict(orient="records")
+
+@app.get("/total-file-length")
+def get_total_length(bq_client: bigquery.client.Client = Depends(get_bq_client)):
+    """
+    Handles GET requests to the gcs-query URL and queries BigQuery.
+    """
+    query = f"""
+            select count(*) as `Total file length`
+            from `project2-cloudpipeline.sales_dataset.sales-data`
+            """
+    try:
+        bq_client = bigquery.Client()
+        query_job = bq_client.query(query)
+        result = query_job.result().to_dataframe()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"BigQuery query failed: {e}")
+    return result.to_dict(orient="records")
 
 @app.post("/convert")
 def post_root():
-    FILE_PATHS = [os.getenv("SALES_CSV1"), os.getenv("SALES_CSV2"), os.getenv("SALES_CSV3"), os.getenv("SALES_CSV4")]
+    FILE_PATHS = [os.getenv("SALES_CSV1"), os.getenv("SALES_CSV2"), os.getenv("SALES_CSV3"), os.getenv("SALES_CSV4"), os.getenv("SALES_CSV5")]
     OUTPUT_FILE = os.getenv("PARQUET_FILE")
     csv_to_parquet(FILE_PATHS, OUTPUT_FILE)
     parquet_to_gcs(OUTPUT_FILE)
